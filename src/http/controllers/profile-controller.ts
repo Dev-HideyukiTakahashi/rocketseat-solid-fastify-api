f56966d353aef5a016783be6245aad3fe9a41fe3
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2026-06-26 04:49:33 -0300

    feat: add middleware jwt verify

--- src/http/controllers/profile-controller.ts
+++ src/http/controllers/profile-controller.ts
@@ -1,7 +1,14 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { makeGetUserProfileService } from '../services/factories/make-get-user-profile-service';
 
 export async function profileController(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify();
+  const getUserProfile = makeGetUserProfileService();
+  const { user } = await getUserProfile.execute({ userId: request.user.sub });
 
-  return reply.status(200).send();
+  return reply.status(200).send({
+    user: {
+      ...user,
+      passwordHash: undefined,
+    },
+  });
 }
